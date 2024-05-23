1dc7151270ce8de82e9bb84b0b3ad67b8da627d1
tiagocomba <combatiago@proton.me>
2024-05-23 19:37:26 -0300

    Estructura consolidada

---
+++ app/Views/home/home.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NVS</title>
+    <link rel="stylesheet" href="<?php echo base_url('complements/styles/home.css'); ?>">
+</head>
+
+<body>
+    <nav>
+        <ul>
+            <li><a href="#features">Software</a>
+                <ul class="dropdown">
+                    <li><a href="#what">¿Qué es?</a></li>
+                    <li><a href="#who">¿A quién está destinado?</a></li>
+                    <li><a href="#how">¿Cómo surgió?</a></li>
+                </ul>
+            </li>
+            <li><a href="#about">About Us</a>
+                <ul class="dropdown">
+                    <li><a href="#creators">Creadores</a></li>
+                    <li><a href="#why">¿Para qué lo hicimos?</a></li>
+                </ul>
+            </li>
+            <li><a href="#contact">Menu</a>
+                <ul class="dropdown">
+                    <li><a href="#console">Consola</a></li>
+                    <li><a href="#login">Iniciar Sesión</a></li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
+    <div class="container">
+        <h1>NVS</h1>
+    </div>
+</body>
+
+</html>
