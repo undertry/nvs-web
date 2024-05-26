3870e550957ab5d01c9d9856ac7a117cfab7f61c
tiagocomba <combatiago@proton.me>
2024-05-26 19:47:58 -0300

    home pre-finalizado

--- app/Views/home/home.php
+++ app/Views/home/home.php
@@ -10,29 +10,41 @@
 
 <body>
     <nav>
+        <div class="logo">NVS</div>
         <ul>
-            <li><a href="#software">Software</a>
+            <li><a href="#features">Software</a>
                 <ul class="dropdown">
-                    <li><a href="#what">what is this?</a></li>
-                    <li><a href="#who">who is it intended for?</a></li>
-                    <li><a href="#how">how did it come about?</a></li>
+                    <li><a href="#what-is">¿Qué es?</a></li>
+                    <li><a href="#who-for">¿A quién está destinado?</a></li>
+                    <li><a href="#origin">¿Cómo surgió?</a></li>
                 </ul>
             </li>
             <li><a href="#about">About Us</a>
                 <ul class="dropdown">
-                    <li><a href="#creators">creators</a></li>
+                    <li><a href="#creators">Los Creadores</a></li>
+                    <li><a href="#purpose">¿Para qué lo hicimos?</a></li>
                 </ul>
             </li>
             <li><a href="#menu">Menu</a>
                 <ul class="dropdown">
-                    <li><a href="#console">console</a></li>
-                    <li><a href="#login">login</a></li>
+                    <li><a href="#console">Consola</a></li>
+                    <li><a href="#login">Iniciar Sesión</a></li>
                 </ul>
             </li>
         </ul>
     </nav>
-    <div class="container">
-        <h1>NVS</h1>
+    <div class="intro">
+        <div class="intro-text">
+            <h1>Analizador de Vulnerabilidades de Red</h1>
+            <p>Este proyecto se centra en analizar vulnerabilidades de una red WiFi entre otro apartados...</p>
+            <div class="buttons">
+                <a href="#" class="btn">Más información</a>
+                <a href="#" class="btn">Ver Repositorio</a>
+            </div>
+        </div>
+        <div class="intro-image">
+            <img src="<?php echo base_url('complements/styles/images/kali.jpg'); ?>" alt="Interfaz de Kali Linux">
+        </div>
     </div>
 </body>
 
