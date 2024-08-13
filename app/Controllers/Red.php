482227a3b52ea3c2ec88b8a13a7e8d276041841a
EzequielMonteverde <cristianmonteverde@alumnos.itr3.edu.ar>
2024-08-13 19:30:57 -0300

    datos duplicados en el array de redes

--- app/Controllers/Red.php
+++ app/Controllers/Red.php
@@ -6,22 +6,29 @@ use CodeIgniter\HTTP\CURLRequest;
 
 use App\Models\RedModel;
 
-class Red extends BaseController
+class RedModel extends Model
 {
-    public function index()
+    protected $table = 'network';
+    protected $primaryKey = 'id_red';
+    protected $allowedFields = ['signal', 'essid', 'bssid', 'encryption', 'channel'];
+
+    public function red($networks)
     {
-        $redModel = new RedModel();
+        // Filtrar redes duplicadas dentro del mismo array
+        $networks = array_unique($networks, SORT_REGULAR);
 
-        $client = \Config\Services::curlrequest();
-        $response = $client->get('http://192.168.0.164:5000/scan');
+        foreach ($networks as $network) {
+            // Verificar si ya existe una red con los mismos valores de bssid, channel, encryption, y essid
+            $existingNetwork = $this->where('bssid', $network['bssid'])
+                                    ->where('channel', $network['channel'])
+                                    ->where('encryption', $network['encryption'])
+                                    ->where('essid', $network['essid'])
+                                    ->first();
 
-        if ($response->getStatusCode() == 200) {
-            $networks = json_decode($response->getBody(), true);
-            $redModel->red($networks);
-        } else {
-            $networks = [];
+            // Si no existe, insertar la nueva red
+            if (!$existingNetwork) {
+                $this->insert($network);
+            }
         }
-
-        return view('red', ['networks' => $networks]);
     }
 }
