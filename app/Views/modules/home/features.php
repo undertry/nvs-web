7243641f82a7fe767467bfe27d3d3c475b2dc1c9
zzzizy89 <zzzizy@proton.me>
2024-09-18 19:42:06 -0300

    changed css section features

--- app/Views/modules/home/features.php
+++ app/Views/modules/home/features.php
@@ -8,34 +8,42 @@
          <h3>FEATURES & SECURITY</h3>
          <div class="feature-item">
              <div class="feature-text hidden">
-                 <h4>Highly Secure</h4>
+                 <h4><i class="fa-solid fa-user-lock"></i> Highly Secure</h4>
                  <p>Passwords are hashed using Bcrypt.</p>
              </div>
          </div>
          <div class="feature-item">
              <div class="feature-text hidden">
-                 <h4>Local Deployment</h4>
+                 <h4><i class="fa-solid fa-server"></i> Local Deployment</h4>
                  <p>Runs locally for enhanced security.</p>
              </div>
          </div>
          <div class="feature-item">
              <div class="feature-text hidden">
-                 <h4>Downloadable Reports</h4>
+                 <h4><i class="fa-solid fa-download"></i> Downloadable Reports</h4>
                  <p>Delete and download scan history in PDF format.</p>
              </div>
          </div>
          <div class="feature-item">
              <div class="feature-text hidden">
-                 <h4>Advanced Security Features</h4>
+                 <h4><i class="fa-solid fa-key"></i> Advanced Security Features</h4>
                  <p>Supports two-factor authentication, password recovery, and change password functionality.</p>
              </div>
          </div>
          <div class="feature-item">
              <div class="feature-text hidden">
-                 <h4>Compatibility</h4>
+                 <h4><i class="fa-brands fa-raspberry-pi"></i> Compatibility</h4>
                  <p>Compatible with Raspberry Pi 3 B+ and later versions.</p>
              </div>
          </div>
+
+         <div class="feature-item">
+             <div class="feature-text hidden">
+                 <h4><i class="fa-solid fa-gear"></i> Highly Configurable</h4>
+                 <p>Customize and adjust settings according to your specific needs in network auditing.</p>
+             </div>
+         </div>
+
      </div>
  </section>
  <hr>
