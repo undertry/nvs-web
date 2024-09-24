a2dc6038b8fa60cf1357632bfdfc378eb54a3632
zzzizy89 <zzzizy@proton.me>
2024-09-24 13:14:52 -0300

    changed about css

--- app/Views/modules/home/about.php
+++ app/Views/modules/home/about.php
@@ -1,49 +1,49 @@
-<div class="section-header hidden">
+<div class="section-header hidden" id="about">
     <h2><i class="fa-solid fa-comments"></i></h2>
 </div>
 
 <section class="about-comments">
-    <h2>An enterprise-ready app that's a joy to use</h2>
+    <h2>What do they think about @NVS?</h2>
     <div class="comments-grid">
         <div class="comment-card">
-            <p class="comment">"The @NVS software has significantly enhanced my security by identifying network vulnerabilities. This has been particularly useful for protecting my extensive computer systems."</p>
+            <p class="comment">"The <span>@NVS</span> software has significantly enhanced my security by identifying network vulnerabilities. This has been particularly useful for protecting my extensive computer systems."</p>
             <h3>schwgon</h3>
-            <p class="occupation">Engineer</p>
+            <p class="occupation">System Engineer</p>
         </div>
 
         <div class="comment-card">
-            <p class="comment">"I find this product to be excellent in fulfilling its promises. Despite still being in development, it is even better than some paid programs with the same purpose. Thank you @NVS!"</p>
+            <p class="comment">"I find this product to be excellent in fulfilling its promises. Despite still being in development, it is even better than some paid programs with the same purpose. Thank you <span>@NVS</span>!"</p>
             <h3>Dr-Cristian</h3>
             <p class="occupation">CEO @ Github</p>
         </div>
 
         <div class="comment-card">
-            <p class="comment">"A well-conceived idea from the initial @NVS design , it effectively meets its objectives and is free of defects. Its creators are visionary and proactive individuals."</p>
+            <p class="comment">"A well-conceived idea from the initial <span>@NVS</span> design , it effectively meets its objectives and is free of defects. Its creators are visionary and proactive individuals."</p>
             <h3>LaureanoCarlos</h3>
             <p class="occupation">Developer Advocate @ supabase</p>
         </div>
         <div class="comment-card">
-            <p class="comment">"The website is very pleasant, easy to understand, and visually appealing. The interaction with it is logical and comprehensible. Regarding the @NVS, it is a very interesting idea that is well implemented."</p>
+            <p class="comment">"The website is very pleasant, easy to understand, and visually appealing. The interaction with it is logical and comprehensible. Regarding the <span>@NVS</span>, it is a very interesting idea that is well implemented."</p>
             <h3>Craifran</h3>
             <p class="occupation">Engineer</p>
         </div>
 
         <div class="comment-card">
-            <p class="comment">"Very good service, very simple and practical. Thanks @NVS!</p>
+            <p class="comment">"Very good service, very simple and practical. Thanks <span>@NVS</span>!</p>
             <h3>Randalfd</h3>
             <p class="occupation">CEO @ Github</p>
         </div>
 
         <div class="comment-card">
-            <p class="comment">"The tool appears to be very useful for addressing current security concerns and frequent attacks. With this device, vulnerabilities are mitigated, and its advanced technology effectively handles highly specific issues. A great idea @NVS!"</p>
+            <p class="comment">"The tool appears to be very useful for addressing current security concerns and frequent attacks. With this device, vulnerabilities are mitigated, and its advanced technology effectively handles highly specific issues. A great idea <span>@NVS</span>!"</p>
             <h3>TadeoBoglione</h3>
             <p class="occupation">Developer Advocate @ supabase</p>
         </div>
 
         <div class="comment-card naahueel">
-            <p class="comment">"I found the site dynamic, interactive and with good navigation flow. Fantastic @NVS.</p>
+            <p class="comment">"I found the site dynamic, interactive and with good navigation flow. Fantastic <span>@NVS</span>.</p>
             <h3>Naahuuel</h3>
-            <p class="occupation">Developer Advocate @ supabase</p>
+            <p class="occupation">Chemical Engineer</p>
         </div>
     </div>
 </section>
