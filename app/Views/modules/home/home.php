b0942b8243d38e63b1ab9e212fd1bcdc6790503a
zzzizy89 <zzzizy@proton.me>
2024-10-01 13:17:55 -0300

    changed css footer and faq

--- app/Views/modules/home/home.php
+++ app/Views/modules/home/home.php
@@ -9,55 +9,56 @@
                 </h3>
             </div>
         </div>
+
     </section>
 
     <script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
     <script>
-    particlesJS('particles-js', {
-        particles: {
-            number: {
-                value: 100
-            },
-            color: {
-                value: '#ffffff'
-            },
-            shape: {
-                type: 'circle'
-            },
-            opacity: {
-                value: 0.5,
-                random: false
-            },
-            size: {
-                value: 3,
-                random: true
-            },
-            line_linked: {
-                enable: true,
-                distance: 150,
-                color: '#ffffff',
-                opacity: 0.4,
-                width: 1
-            },
-            move: {
-                speed: 1,
-                random: true,
-                direction: 'none',
-                out_mode: 'out',
-            }
-        },
-        interactivity: {
-            events: {
-                onhover: {
-                    enable: true,
-                    mode: 'grab'
+        particlesJS('particles-js', {
+            particles: {
+                number: {
+                    value: 100
                 },
-                onclick: {
-                    enable: true,
-                    mode: 'push'
+                color: {
+                    value: '#ffffff'
                 },
-            }
-        },
-        retina_detect: true
-    });
+                shape: {
+                    type: 'circle'
+                },
+                opacity: {
+                    value: 0.5,
+                    random: false
+                },
+                size: {
+                    value: 3,
+                    random: true
+                },
+                line_linked: {
+                    enable: true,
+                    distance: 150,
+                    color: '#ffffff',
+                    opacity: 0.4,
+                    width: 1
+                },
+                move: {
+                    speed: 1,
+                    random: true,
+                    direction: 'none',
+                    out_mode: 'out',
+                }
+            },
+            interactivity: {
+                events: {
+                    onhover: {
+                        enable: true,
+                        mode: 'grab'
+                    },
+                    onclick: {
+                        enable: true,
+                        mode: 'push'
+                    },
+                }
+            },
+            retina_detect: true
+        });
     </script>
