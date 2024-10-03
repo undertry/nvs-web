7322e19c63c3f3641c0f7c143aacbea010499c46
zzzizy89 <zzzizy@proton.me>
2024-10-03 13:30:02 -0300

    implemented change lenguage in section software

--- app/Views/modules/home/home.php
+++ app/Views/modules/home/home.php
@@ -3,7 +3,7 @@
         <div id="particles-js"></div>
         <div class="intro" id="intro">
             <div class="intro-text hidden">
-                <h1 class="title-animate" data-i18n="home.title">NETWORK VULNERABILITY SCAN</h1>
+                <h1 class="title-animate" data-i18n="home.title">NETWORK VULNERABILITY SCANNER</h1>
                 <h3 class="intro-subtext" data-i18n="home.subtext">
                     For <span id="dynamic-word" class="word-slide hidden" data-i18n="home.dynamicWord">passionate</span> people.
                 </h3>
