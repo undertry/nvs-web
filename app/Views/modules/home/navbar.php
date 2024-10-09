6783e5a6e5fbfb9f57f50eaabf9d6f0e4de6b974
zzzizy89 <zzzizy@proton.me>
2024-10-09 08:41:36 -0300

    fixed home

--- app/Views/modules/home/navbar.php
+++ app/Views/modules/home/navbar.php
@@ -7,8 +7,8 @@
             <a href="#home" class="link">
                 <span class="mask">
                     <div class="link-container">
-                        <span class="link-title1 title" data-i18n="navbar.home.link">Home</span>
-                        <span class="link-title2 title" data-i18n="navbar.home.link">Home</span>
+                        <span class="link-title1 title">Home</span>
+                        <span class="link-title2 title">Home</span>
                     </div>
                 </span>
             </a>
@@ -17,8 +17,8 @@
             <a href="#software" class="link">
                 <span class="mask">
                     <div class="link-container">
-                        <span class="link-title1 title" data-i18n="navbar.software.link">Software</span>
-                        <span class="link-title2 title" data-i18n="navbar.software.link">Software</span>
+                        <span class="link-title1 title">Software</span>
+                        <span class="link-title2 title">Software</span>
                     </div>
                 </span>
             </a>
@@ -27,8 +27,8 @@
             <a href="#about" class="link">
                 <span class="mask">
                     <div class="link-container">
-                        <span class="link-title1 title" data-i18n="navbar.about.link">About</span>
-                        <span class="link-title2 title" data-i18n="navbar.about.link">About</span>
+                        <span class="link-title1 title">About</span>
+                        <span class="link-title2 title">About</span>
                     </div>
                 </span>
             </a>
@@ -37,34 +37,21 @@
             <a href="#faq" class="link">
                 <span class="mask">
                     <div class="link-container">
-                        <span class="link-title1 title" data-i18n="navbar.faq.link">Faq</span>
-                        <span class="link-title2 title" data-i18n="navbar.faq.link">Faq</span>
+                        <span class="link-title1 title">Faq</span>
+                        <span class="link-title2 title">Faq</span>
                     </div>
                 </span>
             </a>
         </li>
-        <li class="nav-item"><a href="<?= base_url('login-animation'); ?>" class="login link" data-i18n="navbar.login.link"> Log
+        <li class="nav-item"><a href="<?= base_url('login-animation'); ?>" class="login link"> Log
                 In</a></li>
-        <li class="nav-item signup"><a href="<?= base_url('signup-animation'); ?>" class="signup link" data-i18n="navbar.signup.link">Get Started <span
+        <li class="nav-item signup"><a href="<?= base_url('signup-animation'); ?>" class="signup link">Get Started <span
                     class="arrow">↗</span></a></li>
     </ul>
 
 
     <div class="nav-right">
         <a href="#" id="toggle-dark-mode"><i class="fa-solid fa-moon" id="mode-icon"></i></a>
-        <div class="dropdown">
-            <a href="#" class="globe">
-                <i class="fa-solid fa-globe"></i>
-                <span class="language-label">EN</span>
-                <i class="fa-solid fa-chevron-down"></i>
-            </a>
-            <ul class="dropdown-menu">
-                <li><a href="#" data-lang="en" data-i18n="navbar.languageDropdown.options.en">English</a></li>
-                <li><a href="#" data-lang="es" data-i18n="navbar.languageDropdown.options.es">Spanish</a></li>
-            </ul>
-        </div>
     </div>
-
-
 </nav>
 <!-- navbar section ends here -->
