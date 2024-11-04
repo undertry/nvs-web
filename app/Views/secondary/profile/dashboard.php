364ed2f048d12c715b9931832534fd04549650dc
zzzizy <zzzizy@proton.me>
2024-11-04 00:40:21 -0300

    added arrow in list-items

--- app/Views/secondary/profile/dashboard.php
+++ app/Views/secondary/profile/dashboard.php
@@ -91,20 +91,32 @@
 
 <div class="dashboard">
   <!-- Tarjeta grande a la derecha -->
+
+  <!-- Tarjeta grande a la derecha -->
   <div class="card-large">
-  <h2>View your Wifi Networks</h2>
-  <div id="loading-spinner" style="display: none; text-align: center;">
-    <div class="spinner-border text-primary" role="status"></div>
-    <p><i class="fa-solid fa-spinner"></i></p>
+    <h2>View your Wifi Networks</h2>
+    <div id="loading-spinner" style="display: none; text-align: center;">
+      <div class="spinner-border text-primary" role="status"></div>
+      <p><i class="fa-solid fa-spinner"></i></p>
+    </div>
+    <ul id="wifi-list" class="list-group mt-3" style="display: none;"></ul>
+    <button id="fetch-networks" class="btn btn-primary toggle-btn">
+      <i class="fa-solid fa-chevron-down arrow-icon"></i>
+    </button>
+    <hr class="hr">
+
+    <h2>View your Devices</h2>
+    <div id="loading-spinner-device" style="display: none; text-align: center;">
+      <div class="spinner-border text-primary" role="status"></div>
+      <p><i class="fa-solid fa-spinner"></i></p>
+    </div>
+    <ul id="device-list" class="list-group2 mt-3" style="display: none;"></ul>
+    <button id="fetch-devices" class="btn btn-secondary toggle-btn">
+      <i class="fa-solid fa-chevron-down arrow-icon"></i>
+    </button>
   </div>
-  <ul id="wifi-list" class="list-group mt-3"></ul>
-  <button id="fetch-networks" class="btn btn-primary"><i class="fa-solid fa-magnifying-glass"></i></button>
-  <hr class="hr">
-  <div class="extra-section">
-    <!-- Aquí puedes añadir el contenido adicional debajo de la lista de redes -->
-    <p>Información adicional o una sección extra.</p>
-  </div>
-</div>
+
+
 
 
   <!-- Tarjetas pequeñas a la izquierda -->
@@ -156,55 +168,61 @@
 </script>
 
 <script>
-    document.getElementById('fetch-networks').addEventListener('click', function() {
-      const wifiList = document.getElementById('wifi-list');
-      const loadingSpinner = document.getElementById('loading-spinner');
-  
-      // Mostrar el spinner y limpiar la lista de redes
-      loadingSpinner.style.display = 'block';
-      wifiList.innerHTML = '';
-  
-      fetch('fetchNetworks')
-          .then(response => response.json())
-          .then(data => {
-              loadingSpinner.style.display = 'none'; // Ocultar el spinner
-  
-              if (!data.success) {
-                  alert(data.message);
-                  return;
-              }
-  
-              // Mostrar redes en la lista si la respuesta es exitosa
-              data.data.forEach(network => {
-                  const listItem = document.createElement('li');
-                  listItem.className = 'list-group-item';
-              
-  
-                  const detailsDiv = document.createElement('div');
-                  detailsDiv.className = 'network-details';
-                  detailsDiv.innerHTML = `
-                  <p><strong><i class="fa-solid fa-wifi"></i> ESSID:</strong> ${network.ESSID}</p>
-                  <p><strong><i class="fa-solid fa-tag"></i> BSSID:</strong> ${network.BSSID}</p>
-                  <p><strong><i class="fa-solid fa-list"></i> Canal:</strong> ${network.Channel}</p>
-                  <p><strong><i class="fa-solid fa-signal"></i> Frecuencia:</strong> ${network.Signal}</p>
-                  <p><strong><i class="fa-solid fa-lock"></i> Encryption:</strong> ${network.Encryption}</p>
-                  <button class="btn btn-primary select-network-btn">Seleccionar Red WiFi</button>
-              `;
-  
-                  detailsDiv.querySelector('.select-network-btn').addEventListener('click', function() {
-                      selectNetwork(network);
-                  });
-  
-                  listItem.appendChild(detailsDiv);
-                  wifiList.appendChild(listItem);
-              });
-          })
-          .catch(error => {
-              loadingSpinner.style.display = 'none'; // Ocultar el spinner si ocurre un error
-              console.error('Error al obtener las redes:', error);
-              alert('Error al conectar con la API. Por favor, asegúrate de que está inicializada.');
-          });
-  });
+  document.getElementById('fetch-networks').addEventListener('click', function() {
+    const wifiList = document.getElementById('wifi-list');
+    const loadingSpinner = document.getElementById('loading-spinner');
+    const arrowIcon = this.querySelector('.arrow-icon');
+
+    // Mostrar u ocultar la lista de redes y rotar la flecha
+    if (wifiList.style.display === 'none') {
+        wifiList.style.display = 'block';
+        arrowIcon.classList.add('rotate');
+        loadingSpinner.style.display = 'block';
+        wifiList.innerHTML = '';
+
+        fetch('fetchNetworks')
+            .then(response => response.json())
+            .then(data => {
+                loadingSpinner.style.display = 'none';
+
+                if (!data.success) {
+                    alert(data.message);
+                    return;
+                }
+
+                data.data.forEach(network => {
+                    const listItem = document.createElement('li');
+                    listItem.className = 'list-group-item';
+
+                    const detailsDiv = document.createElement('div');
+                    detailsDiv.className = 'network-details';
+                    detailsDiv.innerHTML = `
+                      <p><strong><i class="fa-solid fa-wifi"></i> ESSID:</strong> ${network.ESSID}</p>
+                      <p><strong><i class="fa-solid fa-tag"></i> BSSID:</strong> ${network.BSSID}</p>
+                      <p><strong><i class="fa-solid fa-list"></i> Canal:</strong> ${network.Channel}</p>
+                      <p><strong><i class="fa-solid fa-signal"></i> Frecuencia:</strong> ${network.Signal}</p>
+                      <p><strong><i class="fa-solid fa-lock"></i> Encryption:</strong> ${network.Encryption}</p>
+                      <button class="btn btn-primary select-network-btn">Seleccionar Red WiFi</button>
+                    `;
+
+                    detailsDiv.querySelector('.select-network-btn').addEventListener('click', function() {
+                        selectNetwork(network);
+                    });
+
+                    listItem.appendChild(detailsDiv);
+                    wifiList.appendChild(listItem);
+                });
+            })
+            .catch(error => {
+                loadingSpinner.style.display = 'none';
+                console.error('Error al obtener las redes:', error);
+                alert('Error al conectar con la API. Por favor, asegúrate de que está inicializada.');
+            });
+    } else {
+        wifiList.style.display = 'none';
+        arrowIcon.classList.remove('rotate');
+    }
+});
   
   
   // Función para enviar la red seleccionada al controlador
@@ -232,4 +250,54 @@
           })
           .catch(error => console.error('Error al seleccionar la red:', error));
   }
+</script>
+
+<script>
+  // Función para alternar la visualización de la lista de dispositivos y la rotación de la flecha
+document.getElementById('fetch-devices').addEventListener('click', function() {
+    const deviceList = document.getElementById('device-list');
+    const loadingSpinnerDevice = document.getElementById('loading-spinner-device');
+    const arrowIcon = this.querySelector('.arrow-icon');
+
+    // Mostrar u ocultar la lista de dispositivos y rotar la flecha
+    if (deviceList.style.display === 'none') {
+        deviceList.style.display = 'block';
+        arrowIcon.classList.add('rotate');
+        loadingSpinnerDevice.style.display = 'block';
+        deviceList.innerHTML = '';
+
+        fetch('fetchDevices')
+            .then(response => response.json())
+            .then(data => {
+                loadingSpinnerDevice.style.display = 'none';
+
+                if (!data.success) {
+                    alert(data.message);
+                    return;
+                }
+
+                data.data.forEach(device => {
+                    const listItem = document.createElement('li');
+                    listItem.className = 'list-group-item';
+
+                    const detailsDiv = document.createElement('div');
+                    detailsDiv.className = 'device-details';
+                    detailsDiv.innerHTML = `
+                      <p><strong><i class="fa-solid fa-tag"></i> MAC Address:</strong> ${device.mac_address}</p>
+                      <p><strong><i class="fa-solid fa-map-pin"></i> IP Address:</strong> ${device.ip_address}</p>
+                    `;
+                    listItem.appendChild(detailsDiv);
+                    deviceList.appendChild(listItem);
+                });
+            })
+            .catch(error => {
+                loadingSpinnerDevice.style.display = 'none';
+                console.error('Error al obtener los dispositivos:', error);
+                alert('Error al conectar con la API de dispositivos. Por favor, asegúrate de que está inicializada.');
+            });
+    } else {
+        deviceList.style.display = 'none';
+        arrowIcon.classList.remove('rotate');
+    }
+});
 </script>
